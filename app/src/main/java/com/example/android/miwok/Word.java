cb46768d33222baac57f18ebdeece604f964cf9a
Roopa <kroopa308@yahoo.com>
2018-07-16 17:27:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,72 @@
+package com.example.android.miwok;
+
+/*
+* {@link Word} represents a vocabulary word that the user wants to learn.
+* It contains a default translation and a Miwok translation for that word.
+*/
+public class Word {
+
+    /* Default translation for the word */
+   private String mDefaultTranslation;
+
+    /* Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    /* Image Resource Id for the word */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    /**
+     * Create a new Word object
+     * @param defaultTranslation is the word in the language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Create a new Word object
+     * @param defaultTranslation is the word in the language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     *
+     * @param imageResourceId is the drawable resouorce ID for the image associated with the word
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
+    /*
+    ** Get default translation of the word.
+     */
+    public String getDefaultTranslation(){
+        return mDefaultTranslation;
+    }
+
+    /*
+    ** Get Miwok translation of the word.
+     */
+    public String getMiwokTranslation(){
+        return mMiwokTranslation;
+    }
+
+    /*
+    ** Return the image resource ID of the word.
+     */
+    public int getImageResourceId(){
+        return mImageResourceId;
+    }
+
+    /**
+     ** Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+}
